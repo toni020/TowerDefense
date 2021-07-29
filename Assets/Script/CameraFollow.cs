a521dbcb2f9fc23dd618cfc5650e49bc8403f8dc
NgocTu <ngoctu0297@gmail.com>
2021-07-29 13:26:43 +0700

    first commit

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject CameraFollowObj;
+    Vector3 FollowPOS;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public Transform Player;
+
+    public float smoothX;
+    public float smoothY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //setup the rotation of the sticks here
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical"); 
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -10, 50);
+
+        
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+        Player.rotation = Quaternion.Euler(0, rotY, 0);
+        
+
+
+    }
+
+
+    void LateUpdate() {
+        CameraUpdate();
+    }
+    void CameraUpdate()
+    {
+        //set the target Obj to follow
+        Transform target = CameraFollowObj.transform;
+
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour
+{
+    
+
+    private CharacterController controller;
+    private float moveSpeed = 10f;
+    private bool groundedPlayer;
+    private float gravityValue = 30f;
+    private Vector3 playerVelocity;
+    private Camera cam;
+
+   
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controller = gameObject.AddComponent<CharacterController>();
+        Physics.gravity = new Vector3(0, -gravityValue, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Vector3 lookdir = mousePos - rb.position;
+        Movement();
+    }
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W)) //move forward
+        {
+            transform.Translate(0f, 0f, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)) //move back
+        {
+            transform.Translate(0f, 0f, -moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A)) //move left
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D)) //move right
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0f, 0f);
+        }
+        
+        
+        /*groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * moveSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetKey(KeyCode.Space) && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);*/
+    }
+}
